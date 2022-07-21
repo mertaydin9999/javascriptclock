779385e5a4d426616d1aa4fe466463f0c48c3afb
mertaydin9999 <65414554+mertaydin9999@users.noreply.github.com>
2022-07-21 21:55:47 +0300

    js-tarih

---
+++ clock.js
@@ -0,0 +1,8 @@
+let isim = prompt("Adinizi giriniz:");
+console.log(isim)
+document.querySelector("#myName").innerHTML = isim;
+
+let tarih = document.querySelector("#myClock");
+tarih.innerHTML = Date()
+
+
